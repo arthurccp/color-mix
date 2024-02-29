72f52f1e541b79197edab9cc6eb58bf6c46b8323
Arthur <arthur@macbook-pro-de-arthur.local>
2024-02-29 15:15:03 -0300

    Initial Commit

---
+++ color-mix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  color-mix
+//
+//  Created by Arthur on 29/02/2024.
+//  Copyright © 2024 Arthur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
